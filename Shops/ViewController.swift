b60c71dc61aeb00302af0b539ce8b37994368515
eltn <https://github.com/mkEldor/blackstone>
2018-10-07 16:12:23 +0600

    elti

---
+++ Shops/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Shops
+//
+//  Created by Eldor Makkambayev on 07.10.2018.
+//  Copyright © 2018 Eldor Makkambayev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+  
+  func eltin(){
+    print("chert")
+  }
+
+
+}
+
